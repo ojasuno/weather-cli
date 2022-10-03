7b36de3826528103f136091fcf4c506cf42e807c
ojasuno <ojasuno21@gmail.com>
2022-10-03 23:13:30 +1100

    reqwest

--- src/main.rs
+++ src/main.rs
@@ -7,5 +7,17 @@ fn main() {
     
     let args: String = arg_iterator.collect();
 
-    dbg!(args);
+    let client = reqwest::blocking::Client::new();
+
+    let response = client
+        .get("https://api.waqi.info/search/")
+        .query(&[("token", api_token), ("keyword", args)])
+        .header(reqwest::header::USER_AGENT, "my cli weather app")
+        .send()
+        .expect("a successfull request")
+        .json::<serde_json::Value>()
+        .expect("expected the body to be json");
+
+    dbg!(response);
+
 }
