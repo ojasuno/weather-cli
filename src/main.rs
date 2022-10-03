1425cd23aa235ce0a677d20d7f16619c6f06867b
ojasuno <ojasuno21@gmail.com>
2022-10-03 21:26:29 +1100

    std::env::args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
-    println!("Hello, world!");
+    let api_token = std::env::var("API_TOKEN")
+        .expect("expected there to be an api token");
+
+    let mut arg_iterator = std::env::args();
+    arg_iterator.next();
+    
+    let args: String = arg_iterator.collect();
+
+    dbg!(args);
 }
